6de8c4a0ba6c8d4896eab827182697867d53c178
mniras <126334365+mniras@users.noreply.github.com>
2023-05-06 16:08:39 +0300

    Update checktele.py
    sleep

--- checktele.py
+++ checktele.py
@@ -274,7 +274,7 @@ async def _(event):
             t.join()
             isav = que.get()
             if "Available" in isav:
-                await asyncio.sleep(3)
+                await asyncio.sleep(1)
                 try:
                     await sedthon(functions.channels.UpdateUsernameRequest(
                         channel=ch, username=username))
